abf1255c5b51fc7fbee1a28f152fc1a13872dc4e
MAvi-Rup <avirupmondal19@gmail.comm>
2023-05-21 17:42:04 +0600

    Done

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<script setup>
+import { RouterView } from 'vue-router'
+import TodoHeader from './components/TodoHeader.vue';
+
+</script>
+
+<template>
+  <TodoHeader />
+
+
+  <RouterView />
+</template>
+<style lang="scss">
+@import url('https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;1,300;1,400;1,500&family=Rubik:wght@300;400;500;700&family=Varela+Round&display=swap');
+ 
+
+* {
+  font-family: "Rubik", sans-serif;
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+.container {
+  max-width: 1100px;
+  margin: 0 auto;
+}
+  
+</style>
